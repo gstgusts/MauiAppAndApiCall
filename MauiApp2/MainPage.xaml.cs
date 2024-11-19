d29ad5938cd41b50660618c82bc56068243a90cc
Gusts Linkevics <gusts@gst.lv>
2024-11-19 20:53:04 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MauiApp2.Models;
+using Newtonsoft.Json;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public static string BaseAddress =
+            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5205" : "http://localhost:5205";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{BaseAddress}/api/Student");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+
+            var students = JsonConvert.DeserializeObject<Student[]>(json);
+        }
+    }
+
+}
